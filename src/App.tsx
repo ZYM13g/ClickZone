7e9877d28a6528dd6b59a0a2fc3f2a63db81b280
Your Name <you@example.com>
2025-05-04 18:06:15 +0700

    Твой комментарий, например: добавил конфиг Prettier

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.scss';
+import Header from './Components/Header';
+import Keyboard from './Components/Keyboard';
+import Main from './Components/Main';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Keyboard />
+      <Main />
+    </>
+  );
+}
+
+export default App;
